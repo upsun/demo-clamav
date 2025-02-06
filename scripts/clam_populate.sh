34c2a77fc20bd469daf9c0ee2771042e3c555e8c
Mickael Gaillard <mickael.gaillard@platform.sh>
2025-02-06 18:28:38 +0100

    Add Runtime Operation for scan

---
+++ scripts/clam_populate.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+echo 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*' > "${FOLDER2SCAN}/fake.txt"
--- scripts/clam_scan-clt.sh
+++ scripts/clam_scan-clt.sh
@@ -5,5 +5,4 @@ clamdscan \
     --config-file="${PLATFORM_APP_DIR}/var/etc/clamd.conf" \
     --log="${PLATFORM_APP_DIR}/var/log/scan-clt.log" \
     --move="${FOLDER2MOVE}" \
-    --recursive \
     "${FOLDER2SCAN}"
